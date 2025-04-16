e25da11b833dc0c49e19e59674f0c0beac1bb149
FATAYERJI Ahmad <ahmad.fatayerji@etu.univ-nantes.fr>
2025-04-16 21:38:18 +0200

    placeholder sign in button

--- src/components/Sign-in.tsx
+++ src/components/Sign-in.tsx
@@ -1,8 +1,14 @@
 "use client"
 
-// import { signIn } from "@auth"
-
 import { signIn } from "next-auth/react"
+
 export default function SignIn() {
-    return <button onClick={() => signIn("google")}></button>
-  }
+  return (
+    <button
+      onClick={() => signIn("google")}
+      className="px-4 py-2 bg-blue-600 text-white rounded"
+    >
+      Sign in with Google
+    </button>
+  )
+}
