7a8ccff74e5b7f9d1e49248f1153d00445a4b068
FATAYERJI Ahmad <ahmad.fatayerji@etu.univ-nantes.fr>
2025-04-16 21:38:34 +0200

    auth system to add auth user and add them to the db

--- auth.ts
+++ auth.ts
@@ -1,7 +1,53 @@
 import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { prisma } from "@/lib/prisma"
 
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+export const { handlers, auth } = NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    
+  ],
+
+  session: {
+    strategy: "jwt",
+  },
+
+  callbacks: {
+    async signIn({ profile }) {
+      if (!profile?.email) throw new Error("No profile email")
+
+      await prisma.user.upsert({
+        where: { email: profile.email },
+        create: {
+          email: profile.email,
+          name: profile.name ?? "",
+          avatar: profile.picture ?? "",
+        },
+        update: {
+          name: profile.name ?? "",
+          avatar: profile.picture ?? "",
+        },
+      })
+
+      return true
+    },
+
+    async jwt({ token, user }) {
+      if (user) token.id = user.id
+      return token
+    },
+
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string
+      }
+      return session
+    },
+  },
 })
