da77cef9a00b10a8b8536377ca372c17cf923ac4
FATAYERJI Ahmad <ahmad.fatayerji@etu.univ-nantes.fr>
2025-04-17 15:51:27 +0200

    removed sign in with google image

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -109,7 +109,6 @@ export default function TestingPage() {
         className="w-full px-4 py-2 bg-red-600 hover:bg-red-700 rounded flex items-center justify-center space-x-2"
       >
         <span>Sign in with Google</span>
-        <img src="/google-icon.svg" alt="G" className="w-5 h-5" />
       </button>
     </div>
   )
